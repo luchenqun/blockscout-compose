97163ef7b1593bc483196522d9e7102460a1861a
Luke <luchenqun@qq.com>
2024-07-16 14:50:58 +0800

    add delply.sh

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+CMD=$1
+
+case ${CMD} in
+start)
+    echo "===== start ===="
+    sudo docker-compose -f ethos.yml down
+    sudo docker-compose -f ethos.yml up -d
+    sleep 3
+    sudo docker ps -a
+    echo "===== end ===="
+    ;;
+stop)
+    echo "===== stop ===="
+    sudo docker-compose -f ethos.yml down
+    sleep 3
+    sudo docker ps -a
+    echo "===== end ===="
+    ;;
+reset)
+    echo "===== reset ===="
+    sudo docker-compose -f ethos.yml down
+    sleep 3
+    cd ./services && sudo rm -rf blockscout-db-data logs redis-data stats-db-data
+    sudo docker-compose -f ethos.yml up -d
+    sleep 3
+    sudo docker ps -a
+    echo "===== end ===="
+    ;;
+*)
+    echo "Usage: deploy.sh start | stop | reset"
+    ;;
+esac
