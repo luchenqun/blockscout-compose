cb0cbed7b3715169ca4a50325814473e8d687ea5
Luke <luchenqun@qq.com>
2024-09-20 16:04:34 +0800

    add quarix

--- deploy.sh
+++ deploy.sh
@@ -4,25 +4,25 @@ CMD=$1
 case ${CMD} in
 start)
     echo "===== start ===="
-    sudo docker-compose -f ethos.yml down
-    sudo docker-compose -f ethos.yml up -d
+    sudo docker-compose -f quarix.yml down
+    sudo docker-compose -f quarix.yml up -d
     sleep 3
     sudo docker ps -a
     echo "===== end ===="
     ;;
 stop)
     echo "===== stop ===="
-    sudo docker-compose -f ethos.yml down
+    sudo docker-compose -f quarix.yml down
     sleep 3
     sudo docker ps -a
     echo "===== end ===="
     ;;
 reset)
     echo "===== reset ===="
-    sudo docker-compose -f ethos.yml down
+    sudo docker-compose -f quarix.yml down
     sleep 3
     cd ./services && sudo rm -rf blockscout-db-data logs redis-data stats-db-data && cd ..
-    sudo docker-compose -f ethos.yml up -d
+    sudo docker-compose -f quarix.yml up -d
     sleep 3
     sudo docker ps -a
     echo "===== end ===="
