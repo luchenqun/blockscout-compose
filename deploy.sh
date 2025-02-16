86ad001dc10d9d706e55a3423c72a039c7622acc
Luke <luchenqun@qq.com>
2025-02-16 15:52:01 +0800

    feat: add mud mainnet

--- deploy.sh
+++ deploy.sh
@@ -4,25 +4,25 @@ CMD=$1
 case ${CMD} in
 start)
     echo "===== start ===="
-    sudo docker-compose -f ethos.yml down
-    sudo docker-compose -f ethos.yml up -d
+    sudo docker-compose -f mud.yml down
+    sudo docker-compose -f mud.yml up -d
     sleep 3
     sudo docker ps -a
     echo "===== end ===="
     ;;
 stop)
     echo "===== stop ===="
-    sudo docker-compose -f ethos.yml down
+    sudo docker-compose -f mud.yml down
     sleep 3
     sudo docker ps -a
     echo "===== end ===="
     ;;
 reset)
     echo "===== reset ===="
-    sudo docker-compose -f ethos.yml down
+    sudo docker-compose -f mud.yml down
     sleep 3
     cd ./services && sudo rm -rf blockscout-db-data logs redis-data stats-db-data && cd ..
-    sudo docker-compose -f ethos.yml up -d
+    sudo docker-compose -f mud.yml up -d
     sleep 3
     sudo docker ps -a
     echo "===== end ===="
